3020744d867cb47462c7e9d68539f9bc736dedd5
chiranjeevi <nchiru987@gmail.com>
2020-03-27 15:53:26 +0530

    hey

--- src/haha.java
+++ src/haha.java
@@ -1,18 +1,26 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class haha {
 
+	private static Scanner in;
+
 	public static void main(String[] args) throws FileNotFoundException {
 	
-		System.out.println("hey hey ");
+		System.out.println("hey hey");
 		
-		Scanner in = new Scanner ( new File("Student.txt"));
+		in = new Scanner ( new File("Student.txt"));
+		List<String> Students = new ArrayList<String> ();
 		 
 		while (in.hasNextLine()){
-			System.out.println(in.nextLine());
+			Students.add(in.nextLine());
+		}
+		for (int i =0; i<Students.size(); i++){
+			System.out.println(Students.get(i));
 		}
 
 	}
