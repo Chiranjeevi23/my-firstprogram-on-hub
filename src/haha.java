7e1fe61549325014ada91ae95dcd7e8dcb75a827
chiranjeevi <nchiru987@gmail.com>
2020-03-27 14:22:29 +0530

    new invention

--- src/haha.java
+++ src/haha.java
@@ -1,9 +1,19 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class haha {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 	
 		System.out.println("hey hey ");
+		
+		Scanner in = new Scanner ( new File("Student.txt"));
+		 
+		while (in.hasNextLine()){
+			System.out.println(in.nextLine());
+		}
 
 	}
 
